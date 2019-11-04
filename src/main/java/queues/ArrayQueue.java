d4d8c6f9d1b08ed0e70dc4dcd4ede0148c1ab446
maciekslotwinski <maciekslotwinski@gmail.com>
2019-11-04 15:35:25 +0100

    arrayqueue implementation

--- src/main/java/queues/ArrayQueue.java
+++ src/main/java/queues/ArrayQueue.java
@@ -2,24 +2,50 @@ package queues;
 
 public class ArrayQueue<T> implements MyQueue<T> {
 
-    private final int MAX_SIZE = 10;
+    private static final int MAX_SIZE = 10;
     private int size = 0;
 
     private Object[] array = new Object[MAX_SIZE];
 
     public void offer(T element) {
+        if (size == MAX_SIZE) {
+            throw new IllegalArgumentException(String.format("Maximum stack size: %s has been reached.", MAX_SIZE));
+        }
 
+        // put element on index - current incremented by one
+        array[size++] = element;
     }
 
     public T poll() {
-        return null;
+        // if no elements in queue - return null
+        if (size == 0) {
+            return null;
+        }
+
+        //  set returned value to queue[0]
+        T value = (T) array[0];
+
+        // decrease size - we are removing element
+        size--;
+
+        // move queue 1 place left
+        System.arraycopy(array, 1, array, 0, size);
+
+        // clear last element
+        array[size] = null;
+
+        return value;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+
+        return (T) array[0];
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
--- src/main/java/queues/MyQueue.java
+++ src/main/java/queues/MyQueue.java
@@ -2,12 +2,16 @@ package queues;
 
 public interface MyQueue<T> {
 
+    // add element
     void offer(T element);
 
+    // get element
     T poll();
 
+    // get element without removing
     T peek();
 
+    // check size
     int size();
 
 }
